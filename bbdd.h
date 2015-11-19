b9d7990f0466718ff215d2b0addb438df5de4904
Eduardo Delgado <aesalemolo@gmail.com>
2015-11-19 16:12:19 +0100

    Create bbdd.h

---
+++ bbdd.h
@@ -0,0 +1,34 @@
+/***
+ *
+ */
+#ifndef __BBDD_H
+#define __BBDD_H
+/******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h> 
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+/******************************************************************************/
+struct nodo
+{
+	struct nodo * sgte;
+	char * id;
+	unsigned short puerto;
+	unsigned long ip;
+};
+
+struct base_datos
+{
+	int nnodos;
+	struct nodo * primero;
+};
+/******************************************************************************/
+int crea_bbdd();
+int inserta_registro(char * id, unsigned short puerto, unsigned long ip);
+int consulta_registro(char * id, unsigned short * puerto, unsigned long * ip);
+int elimina_registro(char * id);
+void imprime_bbdd();
+/******************************************************************************/
+#endif
