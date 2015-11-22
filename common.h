d09c0014b2e4fb602838a51173641c7b0b13dcd3
Eduardo Delgado Díaz <aesalemolo@gmail.com>
2015-11-22 14:42:28 +0100

    Cosmetic changes

--- common.h
+++ common.h
@@ -10,20 +10,20 @@
 #include <string.h>
 #include <stdio.h>
 /******************************************************************************/
-#define MAXSIZE 			256
-#define MAXBUFLEN 			264
-#define PORT_SERVER			2222
-#define PORT_CLIENTE		12344
-#define VERSION 			0x01
+#define MAXSIZE 	256
+#define MAXBUFLEN 	264
+#define PORT_SERVER	2222
+#define PORT_CLIENTE	12344
+#define VERSION 	0x01
 
-#define OP_NUEVO_REG		0x01
-#define OP_CONSULTA_REG		0x02
-#define OP_ELIM_REG			0x03
+#define OP_NUEVO_REG	0x01
+#define OP_CONSULTA_REG	0x02
+#define OP_ELIM_REG	0x03
 
-#define OP_OK 				0x00
-#define OP_KO				0x01
+#define OP_OK 		0x00
+#define OP_KO		0x01
 /******************************************************************************/
-struct nuevo_registro		/* 1 + 1 + 2 + 4 + 256 = 264 bytes */
+struct nuevo_registro	/* 1 + 1 + 2 + 4 + 256 = 264 bytes */
 {
 	char version;
 	char op;
@@ -32,14 +32,14 @@ struct nuevo_registro		/* 1 + 1 + 2 + 4 + 256 = 264 bytes */
 	char id[MAXSIZE];
 } __attribute__((packed));
 
-struct respuesta			/* 1 + 1 + 256 = 258 bytes */
+struct respuesta	/* 1 + 1 + 256 = 258 bytes */
 {
 	char version;
 	char op;
 	char mensaje[MAXSIZE];
 } __attribute__((packed));
 
-struct resp_consulta		/* 1 + 1 + 2 + 4 = 8 bytes */
+struct resp_consulta	/* 1 + 1 + 2 + 4 = 8 bytes */
 {
 	char version;
 	char op;
