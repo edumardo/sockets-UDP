0ffe9d74c267a5f345648ed7aefd8586e2dbcd24
edumardo <aesalemolo@gmail.com>
2015-11-27 19:58:52 +0000

    Document end_server function

--- server_func.c
+++ server_func.c
@@ -1,8 +1,9 @@
 #include "server_func.h"
 /******************************************************************************/
 /**
- *
- *
+ * End the server. Delete the database and exit. Now, the sig parameter is the 
+ * signal SIGINT (Ctrl + C).
+ * @param sig: catched signal identifier.
  */
 void end_server(int sig) {
 	printf("\n");
