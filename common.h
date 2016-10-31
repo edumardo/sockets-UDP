663e2903eedce2a8fac7aa88d924fd9cea34102a
Eduardo Delgado Díaz <aesalemolo@gmail.com>
2016-10-31 16:34:13 +0100

    Replace Tabs for spaces

--- common.h
+++ common.h
@@ -10,44 +10,44 @@
 #include <string.h>
 #include <stdio.h>
 /******************************************************************************/
-#define MAXSIZE 	256
-#define MAXBUFLEN 	264
-#define PORT_SERVER	2222
-#define PORT_CLIENTE	12344
-#define VERSION 	0x01
+#define MAXSIZE         256
+#define MAXBUFLEN       264
+#define PORT_SERVER     2222
+#define PORT_CLIENTE    12344
+#define VERSION         0x01
 
-#define OP_NUEVO_REG	0x01
-#define OP_CONSULTA_REG	0x02
-#define OP_ELIM_REG	0x03
+#define OP_NUEVO_REG    0x01
+#define OP_CONSULTA_REG 0x02
+#define OP_ELIM_REG     0x03
 
-#define OP_OK 		0x00
-#define OP_KO		0x01
+#define OP_OK       0x00
+#define OP_KO       0x01
 
-#define VERBOSE		1
-#define NOVERBOSE	0
+#define VERBOSE     1
+#define NOVERBOSE   0
 /******************************************************************************/
-struct nuevo_registro	/* 1 + 1 + 2 + 4 + 256 = 264 bytes */
+struct nuevo_registro /* 1 + 1 + 2 + 4 + 256 = 264 bytes */
 {
-	char version;
-	char op;
-	unsigned short puerto;
-	unsigned long ip;
-	char id[MAXSIZE];
+    char version;
+    char op;
+    unsigned short puerto;
+    unsigned long ip;
+    char id[MAXSIZE];
 } __attribute__((packed));
 
-struct respuesta	/* 1 + 1 + 256 = 258 bytes */
+struct respuesta /* 1 + 1 + 256 = 258 bytes */
 {
-	char version;
-	char op;
-	char mensaje[MAXSIZE];
+    char version;
+    char op;
+    char mensaje[MAXSIZE];
 } __attribute__((packed));
 
-struct resp_consulta	/* 1 + 1 + 2 + 4 = 8 bytes */
+struct resp_consulta /* 1 + 1 + 2 + 4 = 8 bytes */
 {
-	char version;
-	char op;
-	unsigned short puerto;
-	unsigned long ip;
+    char version;
+    char op;
+    unsigned short puerto;
+    unsigned long ip;
 } __attribute__((packed));
 /******************************************************************************/
 int envia_paquete(int sockfd, struct sockaddr_in server_addr, void * paquete, int len, int verbose);
